ce7e3e130b9c791378f3b54c57dcaececf925e41
zoS3 <you@example.com>
2020-03-14 14:28:51 +0900

    flask 見た目以外は完成

--- app.py
+++ app.py
@@ -14,23 +14,20 @@ model.eval()
 def get_prediction(s):
     assert '[MASK]' in s
     input_ids = tokenizer.encode(s, return_tensors="pt")
-
     masked_index = torch.where(input_ids == tokenizer.mask_token_id)[1].tolist()[0]
-
     with torch.no_grad():
         result = model(input_ids)
-
     result_indices = torch.topk(result[0][:, masked_index], k=10).indices[0].tolist()
-    return ' '.join([tokenizer.decode(i) for i in result_indices])
+    return [tokenizer.decode([i]) for i in result_indices]
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         s = request.form['sent']
-        cloze = get_prediction(s)
-        return cloze
-    return render_template('/index.html')
+        clozes = get_prediction(s)
+        return render_template('index.html', clozes=clozes)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
--- client.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-
-if __name__ == '__main__':
-    s = '山田さんが[MASK]を見たのはこれが初めてでした。巨大だった。'
-    res = requests.post('http://127.0.0.1:5000/', data=json.dumps({'sent': s}))
-    print(res.text)
